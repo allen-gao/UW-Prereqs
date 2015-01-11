623f35daf92929d1ff26f13e37c9e62de4023f11
Allen Gao <allen.gao@uwaterloo.ca>
2015-01-10 20:00:23 -0500

    Added basic search functionality

--- prereqsCtrl.js
+++ prereqsCtrl.js
@@ -1,6 +1,14 @@
 var prereqsApp = angular.module('prereqsApp', []);
 
-prereqsApp.controller('prereqsCtrl', function ($scope) {
-	$scope.courses = ['CS135', 'CS136', 'CS246'];
-}
+prereqsApp.controller('prereqsCtrl', function ($scope, $http) {
+	
+	$http.get('https://api.uwaterloo.ca/v2/codes/subjects.json?key=841086bf587e62c3eee2711d22043b27').success(function(data) {
+		$scope.subjects = data.data;
+	});
+	$scope.searchCourse = function(selected_subject) {
+		$http.get('https://api.uwaterloo.ca/v2/courses/' + $scope.selected_subject +'.json?key=841086bf587e62c3eee2711d22043b27').success(function(data) {
+			$scope.courses = data.data;
+		});
+	}
+});
   
