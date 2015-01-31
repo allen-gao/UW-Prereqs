52e8509eaf8c272b549fbb12d18eb02e0176782d
Allen Gao <allen001@live.ca>
2015-01-31 02:50:29 -0500

    Implemented basic features for modal buttons

--- js/courseModalCtrl.js
+++ js/courseModalCtrl.js
@@ -1,10 +1,17 @@
-angular.module('prereqsApp').controller('courseModalCtrl', function ($scope, course) {
+angular.module('prereqsApp').controller('courseModalCtrl', function ($scope, $modalInstance, course) {
 
 	var makeAPICall = function (callString) { // example of callString: '/codes/subjects'
-    	var response = $q.defer();
-    	$http.get('https://api.uwaterloo.ca/v2' + callString + '.json?key=841086bf587e62c3eee2711d22043b27').then(function(data) {
-      		response.resolve(data);
-    	});
-    	return response.promise;
-  	}
+		var response = $q.defer();
+		$http.get('https://api.uwaterloo.ca/v2' + callString + '.json?key=841086bf587e62c3eee2711d22043b27').then(function(data) {
+			response.resolve(data);
+		});
+		return response.promise;
+	}
+	$scope.ok = function () {
+		$modalInstance.dismiss('ok');
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	}
 });
