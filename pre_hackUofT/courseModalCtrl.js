18e8bab71db300697385b67bfffb515f97874275
thadeaus <graham.cooper33@gmail.com>
2015-02-01 09:54:28 -0500

    removed old files

--- pre_hackUofT/courseModalCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('prereqsApp').controller('courseModalCtrl', function ($scope, $http) {
-	$scope.openModal
-	$http.get('https://api.uwaterloo.ca/v2/courses/' + courseID + '.json?key=841086bf587e62c3eee2711d22043b27').success(function(data) {
-		alert('fsdfs');
-		$scope.course = data.data;
-	});
-});
