7ff93296de10115b28ba66d45fd6b2fc5b49032e
thadeaus <graham.cooper33@gmail.com>
2015-02-01 09:37:05 -0500

    reverted some changes to just show preerquisites

--- js/courseModalCtrl.js
+++ js/courseModalCtrl.js
@@ -1,25 +1,42 @@
 angular.module('prereqsApp').controller('courseModalCtrl', function ($scope, $modalInstance, $http, $q, course) {
 	$scope.course = course;
 	//(1of: PHYS112, PHYS122)(2of: PHYS234, PHYS241, PHYS242, PHYS256, PHYS258/PHYS358 PHYS263, PHYS275)
-	$scope.prereqArr = [];
 	var makeAPICall = function (callString) { // example of callString: '/codes/subjects'
 		var response = $q.defer();
 		$http.get('https://api.uwaterloo.ca/v2' + callString + '.json?key=841086bf587e62c3eee2711d22043b27').then(function(data) {
 			response.resolve(data);
 		});
-		return response.promise;
+		return response.promise;nn
+	}
+
+	//parsePre: Array [Num, String, String....] (Empty String) -> NULL
+	//Effects: Logs the elements in the array
+	var parsePre = function (pre, recCall){
+	  var strOfCourses = "";
+	  for(var i = 0; i < pre.length; i++){
+	    if(typeof pre[i] === 'number'){
+	    	strOfCourses += "(" + pre[i] + "of: ";
+	    }
+	    else if(typeof pre[i] === 'string'){
+	      if(i === pre.length - 1){
+	          strOfCourses += pre[i];
+	      }
+	      else {
+	      	  strOfCourses += pre[i] + ", ";
+	      }
+	    }
+	    else {
+	    	strOfCourses += parsePre(pre[i], recCall + 1) + ")";
+	    }
+	  }
+	  return strOfCourses;
 	}
 
 	makeAPICall('/courses/' + course.subject + '/' + course.catalog_number + '/prerequisites').then(function (response) {
 		$scope.prereqsList = response.data.data.prerequisites_parsed;
 		console.log($scope.prereqsList);
+		$scope.prereqsStr = parsePre($scope.prereqsList);
 	});
-
-	function scroll() {
-    	$window.scrollBy(0,50); // horizontal and vertical scroll increments
-    	scrolldelay = setTimeout('pageScroll()',100); // scrolls every 100 milliseconds
-	}
-		
 
 	$scope.close = function () {
 		$modalInstance.dismiss('close');
--- js/prereqParse.js
+++ js/prereqParse.js
@@ -1,23 +1,31 @@
 //parsePre: Array [Num, String, String....] (Empty String) -> NULL
 //Effects: Logs the elements in the array
-var parsePre = function (pre, tabStr){
-    for(var i = 0; i < pre.length; i++){
-      if(typeof pre[i] === 'number'){
-        console.log(tabStr + pre[i] + "of: ");
+var flat = function(lst){
+      var isflat = true;
+      var flattened = lst;
+
+      for(var i = 0; i < lst.length; i++){
+        console.log("FIRST " + lst[i]);
+
+        if(typeof lst[i] === 'object'){
+          isflat = false;
+          console.log("Isflat1: " + isflat);
+        }
       }
-      else if(typeof pre[i] === 'string'){
-        if(i === pre.length - 1){
-            console.log(tabStr + pre[i]);
-        }
-        else {
-          console.log(tabStr + pre[i] + ", ");
-        }
+
+      if(isflat === false){
+        console.log("isFlat2: " + isflat);
+        flattened = lst.reduce(function(a, b) {
+            return a.concat(b);
+        });
+        console.log("flattened: " + flattened);
+        return flat(flattened);
       }
       else {
-        parsePre(pre[i], tabStr + "  ");
+        return flattened;
       }
     }
-  }
+
 var stuff = {
   "subject":"PHYS",
   "catalog_number":"375",
@@ -28,6 +36,9 @@ var stuff = {
                                   [ 1, "PHYS258", "PHYS358"],
                                   "PHYS263", "PHYS275"]]
 };
-var pr = stuff.prerequisites_parsed;
-
-parsePre(pr, "");
+var pr = ['a', 'b', 'c']//stuff.prerequisites_parsed;
+//console.log(pr);
+flattened = pr.reduce(function(a, b) {
+            return a.concat(b);
+        });
+console.log(pr);
