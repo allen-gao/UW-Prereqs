7ee2fab48bb3292fa891b81ad43d7869da5cae81
Allen Gao <allen.gao@uwaterloo.ca>
2015-05-10 16:13:59 -0400

    init with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name prereqsAppApp
+ * @description
+ * # prereqsAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('prereqsAppApp', [
+    'ngCookies',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
